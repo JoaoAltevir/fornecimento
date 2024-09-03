418e1b59c397570683c1dbc08ab13583da35e88c
João Neto <j.altevirneto@gmail.com>
2024-09-03 10:51:07 -0300

    feat: feito os cruds dos modulos

---
+++ src/controllers/fornecedor.js
@@ -0,0 +1,35 @@
+const db = [];
+
+const model = require("../models/fornecedor.js");
+
+const store = body => {
+    const novo = model(body);
+    if(novo){
+        db.push(novo);
+        return 200
+    }
+    return 400
+}
+
+const index = () => db
+
+const show = id => db.find(el => el.id == id)
+
+const update = (body, id) => {
+    const index = db.findIndex(el => el.id == id)
+    const atualizado = model(body, id);
+    if(atualizado && index != -1){
+        db[index] = atualizado;
+        return 200
+    }
+    return 400
+}
+
+const destroy = id => {
+    const index = db.findIndex(el => el.id == id);
+    if(index != -1){
+        db.splice(index,1);
+        return 200
+    }
+    return 400
+}
---
+++ src/controllers/produto.js
@@ -0,0 +1,34 @@
+const db = [];
+const model = require("../models/produto.js");
+
+const store = body => {
+    const novo = model(body);
+    if(novo){
+        db.push(novo);
+        return 200
+    }
+    return 400
+}
+
+const index = () => db
+
+const show = id => db.find(el => el.id == id)
+
+const update = (body, id) => {
+    const index = db.findIndex(el => el.id == id)
+    const atualizado = model(body, id);
+    if(atualizado && index != -1){
+        db[index] = atualizado;
+        return 200
+    }
+    return 400
+}
+
+const destroy = id => {
+    const index = db.findIndex(el => el.id == id);
+    if(index != -1){
+        db.splice(index,1);
+        return 200
+    }
+    return 400
+}
